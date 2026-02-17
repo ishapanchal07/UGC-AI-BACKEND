cd48a3cfb6063d7a9420add3897777b1a338b923
ishapanchal07 <isha7112003@gmail.com>
2026-02-17 12:34:21 +0530

    adding sentry

---
+++ configs/instrument.mjs
@@ -0,0 +1,9 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://23d8d1befe632dc9af3fc786c9a9f3a0@o4510899973193728.ingest.us.sentry.io/4510899984465920",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
