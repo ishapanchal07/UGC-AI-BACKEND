fec6ee7c3f510d707b8b1cde23448d8368dcbc33
ishapanchal07 <isha7112003@gmail.com>
2026-02-17 13:54:43 +0530

    error tracking

--- controllers/clerk.ts
+++ controllers/clerk.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { verifyWebhook } from '@clerk/express/webhooks'
 import { prisma } from "../configs/prisma.js";
+import * as Sentry from "@sentry/node"
+
 
 
 const clerkWebhooks = async (req: Request, res: Response) => 
@@ -70,6 +72,7 @@ const clerkWebhooks = async (req: Request, res: Response) =>
         res.json({message: "Webhook Recieved : " + type})
 
     } catch (error: any) {
+        Sentry.captureException(error)
         res.status(500).json({ message: error.message });
     }
 }
