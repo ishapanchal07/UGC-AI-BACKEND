769c860f7930c2151ce079fefa8961847b7e2314
ishapanchal07 <isha7112003@gmail.com>
2026-02-18 10:47:47 +0530

    create api

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
 import * as Sentry from "@sentry/node";
+import { prisma } from "../configs/prisma.js";
 
 
 // Get User Credits
 export const getUserCredits = async (req: Request, res: Response) =>{
     try {
-        
+        const {userId} = req.auth();
+        if(!userId) {return res.status(401).json({message: 'Unauthorized'})}
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId }
+        })
+
+        res.json({credits: user?.credits})
+
     } catch (error: any) {
         Sentry.captureException(error);
         res.status(500).json({message: error.code || error.message})
@@ -15,22 +24,39 @@ export const getUserCredits = async (req: Request, res: Response) =>{
 // const get all user projects
 export const getAllProjects = async (req: Request, res: Response) =>{
     try {
-        
+        const {userId} = req.auth();
+        const projects = await prisma.project.findMany({
+            where: {userId},
+            orderBy: {createdAt: 'desc'}
+        })
+        res.json({projects})
     } catch (error: any) {
         Sentry.captureException(error);
         res.status(500).json({message: error.code || error.message})
     }
 }
 
-// Get project by id
 export const getProjectById = async (req: Request, res: Response) =>{
     try {
-        
+
+        const { userId } = req.auth();
+        const projectId = req.params.projectId as string;
+
+        const project = await prisma.project.findFirst({
+            where: { id: projectId, userId },
+        })
+
+        if(!project) { 
+            return res.status(404).json({ message: 'Project not found' }) 
+        }
+
+        res.json({ project })
     } catch (error: any) {
         Sentry.captureException(error);
-        res.status(500).json({message: error.code || error.message})
+        res.status(500).json({ message: error.code || error.message })
     }
 }
+
 
 // publish / unpublish project
 export const toggleProjectPublic = async (req: Request, res: Response) =>{
