a7e3e31d3642df8b4eace17345c744772a4f8469
ishapanchal07 <isha7112003@gmail.com>
2026-02-24 11:28:12 +0530

    inatall axios

--- controllers/projectController.ts
+++ controllers/projectController.ts
@@ -149,7 +149,9 @@ export const createProject = async (req: Request, res: Response) => {
 
         const base64Image = `data:image/png;base64,${finalBuffer.toString('base64')}`
 
-        const uploadResult = await cloudinary.uploader(base64Image,{resource_type: 'image'});
+        const uploadResult = await cloudinary.uploader.upload(base64Image, {
+            resource_type: 'image'
+        });
 
         await prisma.project.update({
             where: {id: project.id},
@@ -159,15 +161,74 @@ export const createProject = async (req: Request, res: Response) => {
             }
         })
 
+        res.json({projectId: project.id})
+
     } catch (error: any) {
+        if(tempProjectId!){
+            // update project status and error message
+            await prisma.project.update({
+                where: {id: tempProjectId},
+                data: {isGenerating: false, error: error.message}
+            })
+        }
+
+        if(isCreditDeducted){
+            // add create back
+            await prisma.user.update({
+                where: {id: userId},
+                data: {credits: {increment: 5}}
+            })
+        }
         Sentry.captureException(error);
         res.status(500).json({ message: error.message });
     }
 };
 
 export const createVideo = async (req: Request, res: Response) => {
-    try {
+    const {userId} = req.auth()
+    const { projectId } = req.body;
+    let isCreditDeducted = false;
 
+    const user = await prisma.user.findUnique({
+        where: {id: userId}
+    })
+
+    if(!user || user.credits < 10){
+        return res.status(401).json({ message: 'Insufficient credits'});
+    }
+
+    // deduct credits for video generation
+    await prisma.user.update({
+        where: {id: userId},
+        data: {credits: {decrement: 10}}
+    }).then(()=>{ isCreditDeducted = true});
+
+    try {
+        const project = await prisma.project.findUnique({
+            where: {id: projectId, userId},
+            include: {user: true}
+        })
+
+        if(!project || project.isGenerating){
+            return res.status(404).json({message: 'Generation in progress'});
+        }
+
+        if(project.generatedVideo){
+            return res.status(404).json({message: 'Video already generated'});
+        }
+
+        await prisma.project.update({
+            where: {id: projectId},
+            data: {isGenerating: true}
+        })
+
+        const prompt = `make the showcase the product which is ${project.productName} ${project.productDescription && `ans Product Description: ${project.productDescription}`}`
+
+        const model = 'veo-3.1-generate-preview'
+
+        if(!project.generatedImage){
+            throw new Error('Generated image not found');
+        }
     } catch (error: any) {
         Sentry.captureException(error);
         res.status(500).json({ message: error.message });
